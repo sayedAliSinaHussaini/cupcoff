4064f2773842643f7076bceb5994ccef3ca4988a
sayedAliSinaHussaini <s.alisinahussaini313@gmail.com>
2022-05-29 20:11:47 +0430

    1st version of Cupcoff Completed 1.0.0

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -2,47 +2,67 @@ import 'package:cupcoff/cupcoff.dart';
 import 'package:flutter/material.dart';
 
 void main(List<String> args) {
-  runApp(MaterialApp(
-    home: Scaffold(
+  runApp(const HomePage());
+}
+
+//this is an example of how to work with the CUPCOFF package.
+class MyApp extends StatelessWidget {
+  const MyApp({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: HomePage());
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       body: CupCoff(
-        scrollDirection: Axis.horizontal,
         padding: const EdgeInsets.all(30),
-        margin: const EdgeInsets.all(10),
-        backgrounColor: Colors.red,
-        enableScroll: false,
-        child: Column(children: [
-          Container(
-            height: 10,
-            width: 10,
-            margin: EdgeInsets.all(10),
-            color: Colors.amber,
-          ),
-          Container(
-            height: 10,
-            width: 10,
-            margin: EdgeInsets.all(10),
-            color: Colors.amber,
-          ),
-          Container(
-            height: 10,
-            width: 10,
-            margin: EdgeInsets.all(10),
-            color: Colors.amber,
-          ),
-          Container(
-            height: 10,
-            width: 10,
-            margin: EdgeInsets.all(10),
-            color: Colors.amber,
-          ),
-          Container(
-            height: 10,
-            width: 10,
-            margin: EdgeInsets.all(10),
-            color: Colors.amber,
-          )
-        ]),
+        enableScroll: true,
+        child: SizedBox(
+          child: Column(children: [
+            Container(
+              height: 100,
+              width: 100,
+              margin: const EdgeInsets.all(10),
+              color: Colors.red,
+            ),
+            Container(
+              height: 100,
+              width: 100,
+              margin: const EdgeInsets.all(10),
+              color: Colors.black,
+            ),
+            Container(
+              height: 100,
+              width: 100,
+              margin: const EdgeInsets.all(10),
+              color: Colors.green,
+            ),
+            Container(
+              height: 100,
+              width: 100,
+              margin: const EdgeInsets.all(10),
+              color: Colors.amber,
+            ),
+            Container(
+              height: 100,
+              width: 100,
+              margin: const EdgeInsets.all(10),
+              color: Colors.amber,
+            )
+          ]),
+        ),
       ),
-    ),
-  ));
+    );
+  }
 }
